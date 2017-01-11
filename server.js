31533364b2cf481850d8265d517d481c68839f19
alexisgraff5 <lex.graff@gmail.com>
2017-01-11 16:07:03 -0700

    contact form set up

--- server.js
+++ server.js
@@ -1,17 +1,45 @@
 
-var express = require('express');
-var session = require('express-session');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
 var config = require('./server_config.json');
+
+const app = module.exports = express();
+const EMAIL_ACCOUNT_USER = config.email;
+const EMAIL_ACCOUNT_PASSWORD = config.password;
+const YOUR_NAME = config.name;
 
 var app = express();
 
+app.use(bodyParser.json());
+app.use(cors());
 app.use(express.static('public'));
 
-app.use(session({
-  secret: config.sessionSecret,
-  resave: true,
-  saveUninitialized: true
-}));
+var smtpTransport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: EMAIL_ACCOUNT_USER,
+    pass: EMAIL_ACCOUNT_PASSWORD
+  }
+});
+
+app.post('/email', function(req, res, next) {
+  smtpTransport.sendMail({
+    from: `${YOUR_NAME} ${EMAIL_ACCOUNT_USER}`,
+    to: 'lex.graff@gmail.com',
+    subject: 'Message from Portfolio Site',
+    text: `From: ${req.body.name} at ${req.body.email}. ${req.body.message}, and their phone number is ${req.body.number}`
+  }, function(error, response) {
+    if (error) {
+      console.log(error);
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(200);
+    }
+    smtpTransport.close();
+  });
+});
 
 app.listen(config.port, function() {
   console.log('listening on port ' + config.port);
